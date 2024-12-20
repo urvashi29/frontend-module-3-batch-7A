15ccef0776a4ae3130061b59b8f51c2e710b271a
your_user_namer <urvashigolu29@gmail.com>
2024-12-20 21:53:10 +0530

    added Lesson-20

---
+++ Lesson-20/my-app/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import User from "./User";
+
+//function based component | First letter should be capital
+const App = () => {
+  //jsx -> javascript syntax XML extension
+
+  return (
+    <>
+      <h1>ReactJs</h1>
+      <p>Hello world</p>
+      <User />
+    </>
+  );
+};
+
+export default App;
+
+// component render/display -> return part
+
+// Babel is Transcompiler
+
+// shortcut: rsc + enter
---
+++ Lesson-20/my-app/src/User.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+//class based component
+class User extends Component {
+  render() {
+    return (
+      //jsx
+
+      <ul>
+        <li>User 1</li>
+        <li>User 2</li>
+        <li>User 3</li>
+      </ul>
+    );
+  }
+}
+
+export default User;
+
+// rcc -> class based component
