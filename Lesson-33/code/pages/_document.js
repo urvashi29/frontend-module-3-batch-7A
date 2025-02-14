01717ac3a9d9cd7e9f352d105739410483a274c9
your_user_namer <urvashigolu29@gmail.com>
2025-02-14 21:50:46 +0530

    added notes in Lesson-33

---
+++ Lesson-33/code/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+// index.html of ReactJs
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        {/* <h2>Almabetter</h2> */}
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ Lesson-33/code/pages/index.jsx
@@ -0,0 +1,105 @@
+// // Server Side Rendering
+// import React from "react";
+
+// const Home = ({ products }) => {
+//   console.log(products);
+
+//   const productList = products.map((product) => {
+//     return <p>{product.title}</p>;
+//   });
+
+//   return (
+//     <>
+//       <div>{productList}</div>
+//     </>
+//   );
+// };
+
+// export default Home;
+
+// // This function will call on server side
+// export async function getServerSideProps() {
+//   console.log("get data at server");
+
+//   // you can use axios also
+//   const response = await fetch("https://dummyjson.com/products");
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       products: data.products,
+//     },
+//   };
+// }
+
+// Static Generation
+// export default function StaticComponent({ products }) {
+//   const productList = products.map((product) => {
+//     return <p>{product.title}</p>;
+//   });
+//   return <>{productList}</>;
+// }
+
+// export async function getStaticProps() {
+//   console.log("get static props");
+//   const response = await fetch("https://dummyjson.com/users");
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       products: data.products,
+//     },
+//   };
+// }
+
+// Incremental static generation
+// import path from "path";
+// import fsPromises from "fs/promises";
+
+// export default function StaticComponent({ recipes }) {
+//   const recipeList = recipes.map((recipe) => {
+//     return <p>{recipe.strCategory}</p>;
+//   });
+//   return <>{recipeList}</>;
+// }
+
+// export async function getStaticProps() {
+//   // read a file in backend (nodejs)
+//   console.log("data changed");
+//   const filePath = path.join(process.cwd(), "recipedata/recipes.json");
+//   const data = await fsPromises.readFile(filePath);
+
+//   const recipes = JSON.parse(data);
+
+//   return {
+//     props: {
+//       recipes,
+//     },
+//     // 30 seconds
+//     revalidate: 30,
+//   };
+// }
+
+// client side renderding
+// import React from "react";
+// import { useEffect } from "react";
+
+// const index = () => {
+//   useEffect(() => {}, []);
+//   return <div></div>;
+// };
+
+// export default index;
+
+// SWR (client side rendering)
+import React from "react";
+import { useSWR } from "swr";
+
+const fetcher = (api) => fetch(api).then((res) => res.json());
+const { data } = useSWR("https://dummyjson.com/users", fetcher);
+
+const index = () => {
+  return <div></div>;
+};
+
+export default index;
